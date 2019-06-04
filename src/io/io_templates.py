dfe5184620ec882ec13196b03202efd9643d35f7
Corey Adams <corey.adams@anl.gov>
2019-06-04 13:57:09 -0500

    Add io template tools.

---
+++ src/io/io_templates.py
@@ -0,0 +1,66 @@
+from . import larcv_io
+
+
+
+# Here, we set up a bunch of template IO formats in the form of callable functions:
+
+# These are all doing sparse IO, so there is no dense IO template here.  But you could add it.
+
+def train_io(input_file, max_voxels, data_producer, label_producer, prepend_names=""):
+    data_proc = gen_sparse2d_data_filler(name=prepend_names + "data", producer=data_producer, max_voxels=max_voxels)
+    label_proc = gen_sparse2d_data_filler(name=prepend_names + "label", producer=label_producer, max_voxels=max_voxels)
+
+
+    config = larcv_io.ThreadIOConfig(name="TrainIO")
+
+    config.add_process(data_proc)
+    config.add_process(label_proc)
+
+    config.set_param("InputFiles", input_file)
+
+    return config
+
+
+def test_io(input_file, max_voxels, data_producer, label_producer, prepend_names="aux_"):
+    data_proc = gen_sparse2d_data_filler(name=prepend_names + "data", producer=data_producer, max_voxels=max_voxels)
+    label_proc = gen_sparse2d_data_filler(name=prepend_names + "label", producer=label_producer, max_voxels=max_voxels)
+
+
+    config = larcv_io.ThreadIOConfig(name="TestIO")
+
+    config.add_process(data_proc)
+    config.add_process(label_proc)
+
+    config.set_param("InputFiles", input_file)
+
+    return config
+
+
+def ana_io(input_file, max_voxels, data_producer, label_producer, prepend_names=""):
+    data_proc = gen_sparse2d_data_filler(name=prepend_names + "data", producer=data_producer, max_voxels=max_voxels)
+    label_proc = gen_sparse2d_data_filler(name=prepend_names + "label", producer=label_producer, max_voxels=max_voxels)
+
+
+    config = larcv_io.ThreadIOConfig(name="AnaIO")
+
+    config.add_process(data_proc)
+    config.add_process(label_proc)
+
+    config.set_param("InputFiles", input_file)
+
+    return config
+
+def gen_sparse2d_data_filler(name, producer, max_voxels):
+
+    proc = larcv_io.ProcessConfig(proc_name=name, proc_type="BatchFillerSparseTensor2D")
+
+    proc.set_param("Verbosity",         "3")
+    proc.set_param("Tensor2DProducer",  producer)
+    proc.set_param("IncludeValues",     "true")
+    proc.set_param("MaxVoxels",         max_voxels)
+    proc.set_param("Channels",          "[0,1,2]")
+    proc.set_param("UnfilledVoxelValue","-999")
+    proc.set_param("Augment",           "false")
+
+    return proc
+
---
+++ src/io/larcv_io.py
@@ -0,0 +1,195 @@
+import sys, os
+from collections import OrderedDict
+
+''' 
+The point of this class is to take a loose configuration specification
+and return a properly formated larcv IO configuration.  So, it can spit out
+a full configuration string, or even a temp file.
+
+'''
+
+
+class ProcessConfig(object):
+
+    def __init__(self, proc_name, proc_type):
+        self._name   = proc_name
+        self._type   = proc_type
+        self._params = OrderedDict()
+
+    def set_param(self, key, value):
+        self._params[key] = value
+
+    def str(self, indent_level):
+        # Return the string corresponding to this process's configuration
+        output_str = ""
+        output_str += "{indent}{name}: {{\n".format(indent=" "*indent_level, name=self._name)
+        indent_level += 2
+        for param in self._params:
+            output_str += "{indent}{param}: {value}\n".format(
+                indent = " "*indent_level,
+                param  = param,
+                value  = self._params[param]
+            )
+        indent_level -= 2
+        output_str += "{indent}}}\n".format(indent=" "*indent_level)
+        return output_str
+
+class ProcessListConfig(object):
+
+    def __init__(self):
+        self._processes = []
+
+
+    def str(self, indent_level):
+        # For each process, get the string representation and concatenate them
+        output_str = ""
+        for proc in self._processes:
+            output_str += proc.str(indent_level)
+        return output_str
+
+    def process_types(self):
+        # Return the list of process types, properly formatted
+        return "[{}]".format(",".join(["\"{}\"".format(proc._type) for proc in self._processes]))
+
+    def process_names(self):
+        # Return the list of process names, properly formated
+        return "[{}]".format(",".join(["\"{}\"".format(proc._name) for proc in self._processes]))
+
+    def add_process(self, process):
+        self._processes.append(process)
+
+class CoreConfig(object):
+
+    def __init__(self):
+
+        self._process_list = ProcessListConfig()
+
+    def add_process(self, process):
+        self._process_list.add_process(process)
+
+class IOManagerConfig(object):
+
+
+    def __init__(self):
+        self._params = OrderedDict()
+        self._params["Verbosity"] =  None
+        self._params["IOMode"] =  None
+        self._params["OutFileName"] =  None
+        self._params["InputFiles"] =  None
+        self._params["InputDirs"] =  None
+        self._params["StoreOnlyName"] =  None
+        self._params["StoreOnlyType"] =  None
+        self._params["ReadOnlyName"] =  None
+        self._params["ReadOnlyType"] =  None
+
+# class ProcessDriverConfig(CoreConfig):
+
+#     def __init__(self):
+#         CoreConfig.__init__(self)
+#         self._io_config = IOManagerConfig()
+
+#         self._params = OrderedDict()
+
+#         self._params["ProcessList"] =  None
+#         self._params["RandomAccess"] =  None
+#         self._params["AnaFile"] =  None
+#         self._params["StartEntry"] =  None
+#         self._params["NumEntries"] =  None
+#         self._params["ProcessType"] =  None
+#         self._params["ProcessName"] =  None
+
+#         self._params["IOManager"] =  self._io_config
+
+
+
+
+
+class ThreadIOConfig(CoreConfig):
+
+
+    def __init__(self, name):
+        CoreConfig.__init__(self)
+        self._name = name
+        self._params = OrderedDict()
+
+        self._params["Verbosity"] =  None
+        self._params["EnableFilter"] =  None
+        self._params["NumThreads"] =  None
+        self._params["InputFiles"] =  None
+        self._params["NumBatchStorage"] =  None
+        self._params["RandomSeed"] =  None
+        self._params["RandomAccess"] =  None
+
+
+        self._defaults_set = False
+
+    def set_param(self, param, value):
+        self._params[param] = value
+
+    def set_defaults(self):
+
+        if self._params['Verbosity'] is None:
+            self._params['Verbosity'] = "3"
+        if self._params['EnableFilter'] is None:
+            self._params['EnableFilter'] = "false"
+        if self._params['NumThreads'] is None:
+            self._params['NumThreads'] = "1"
+        if self._params['NumBatchStorage'] is None:
+            self._params['NumBatchStorage'] = "1"
+        if self._params['RandomSeed'] is None:
+            self._params['RandomSeed'] = "0"
+        if self._params['RandomAccess'] is None:
+            self._params['RandomAccess'] = "2"
+
+        self._defaults_set = True
+
+    def generate_config_str(self):
+
+        if not self._defaults_set:
+            self.set_defaults()
+
+        indent_level = 0
+
+        # Take the config and generate a parsable string:
+        output_str = ""
+        output_str += self._name + ": {\n"
+        indent_level += 2
+
+        for param in self._params:
+            if param == 'InputFiles':
+                output_str += "{indent}{param}: [\"{value}\"]\n".format(
+                    indent = " "*indent_level,
+                    param  = param,
+                    value  = self._params[param])
+            else:
+                output_str += "{indent}{param}: {value} \n".format(
+                    indent = " "*indent_level,
+                    param  = param,
+                    value  = self._params[param])
+
+        # Add the list of process types and names:
+        output_str += "{indent}ProcessType: {procs}\n".format(
+            indent=" "*indent_level, procs=self._process_list.process_types())
+        output_str += "{indent}ProcessName: {procs}\n".format(
+            indent=" "*indent_level, procs=self._process_list.process_names())
+
+        # Now, get the processes:
+        output_str += "\n"
+        output_str += "{indent}ProcessList: {{\n".format(indent=" "*indent_level)
+        indent_level += 2
+        output_str += self._process_list.str(indent_level=indent_level)
+        indent_level -= 2
+
+        output_str += "{indent}}}\n".format(indent=" "*indent_level)
+        indent_level -= 2
+        output_str += "{indent}}}\n".format(indent=" "*indent_level)
+
+
+        return output_str
+
+
+
+
+
+
+
