65ed69adab1183b3b8163f084453973cd1923baa
Corey <corey.adams@anl.gov>
2020-04-14 15:43:27 +0000

    This modifies the main executable to make the iotest work without frameworks.
    It also changes the default device from CPU to GPU.

--- bin/exec.py
+++ bin/exec.py
@@ -41,7 +41,6 @@ The most commonly used commands are:
         # use dispatch pattern to invoke method with same name
         getattr(self, args.command)()
 
-
     def add_shared_training_arguments(self, parser):
 
         ##################################################################
@@ -203,15 +202,13 @@ The most commonly used commands are:
         self.args.training = False
         self.args.mode = "iotest"
 
-        print("Running IO Test")
-        
-
         self.make_trainer()
+        
+        self.trainer.print("Running IO Test")
+        self.trainer.print(self.__str__())
+
 
         self.trainer.initialize(io_only=True)
-        
-        # Trainer's print will automatically restrict to one rank in MPI mode:
-        self.trainer.print(self.__str__())
 
         # label_stats = numpy.zeros((36,))
         global_start = time.time()
@@ -219,26 +216,20 @@ The most commonly used commands are:
         for i in range(self.args.iterations):
             start = time.time()
             mb = self.trainer.larcv_fetcher.fetch_next_batch("train", force_pop=True)
-            # print(mb.keys())
-            # label_stats += numpy.sum(mb['label'], axis=0)
 
             end = time.time()
-            # if not self.args.distributed:
-            self.trainer.print(i, ": Time to fetch a minibatch of data: {}".format(end - start))
-            # else:
-            #     if self.trainer._rank == 0:
-            #         print(i, ": Time to fetch a minibatch of data: {}".format(end - start))
-        # print(label_stats)
-        # time.sleep(0.1)
-        self.trainer.print("Total IO Time: ", time.time() - global_start)
 
+            self.trainer.print(i, ": Time to fetch a minibatch of data: {}".format(end - start))
+
+        self.trainerprint("Total IO Time: ", time.time() - global_start)
     def make_trainer(self):
 
         self.validate_arguments()
 
         if self.args.mode == "iotest":
             from src.utils.core import trainercore
-            self.trainer = trainercore.trainercore
+            self.trainer = trainercore.trainercore(self.args)
+            return
 
         if self.args.framework == "tensorflow" or self.args.framework == "tf":
 
@@ -318,7 +309,7 @@ The most commonly used commands are:
         parser.add_argument('-m','--compute-mode',
             type    = str,
             choices = ['CPU','GPU'],
-            default = 'CPU',
+            default = 'GPU',
             help    = "Selection of compute device, CPU or GPU ")
 
         parser.add_argument('-ld','--log-directory',
@@ -340,8 +331,8 @@ The most commonly used commands are:
     def add_io_arguments(self, parser):
 
         # data_directory = "/lus/theta-fs0/projects/datascience/cadams/datasets/SBND/H5/cosmic_tagging/"
-        data_directory = "/Users/corey.adams/data/dlp_larcv3/sbnd_cosmic_samples/cosmic_tagging/"
-        # data_directory = "/gpfs/jlse-fs0/users/cadams/datasets/cosmic_tagging/"
+        # data_directory = "/Users/corey.adams/data/dlp_larcv3/sbnd_cosmic_samples/cosmic_tagging/"
+        data_directory = "/gpfs/jlse-fs0/users/cadams/datasets/cosmic_tagging/"
 
         # IO PARAMETERS FOR INPUT:
         parser.add_argument('-f','--file',
@@ -362,8 +353,8 @@ The most commonly used commands are:
         # IO PARAMETERS FOR AUX INPUT:
         parser.add_argument('--aux-file',
             type    = str,
-            default = None,
-            # default = data_directory + "cosmic_tagging_test.h5",
+            # default = None,
+            default = data_directory + "cosmic_tagging_test.h5",
             help    = "IO Aux Input File, or output file in inference mode")
 
 
@@ -406,7 +397,7 @@ The most commonly used commands are:
         if self.args.framework == "torch":
             # In torch, only option is channels first:
             if self.args.data_format == "channels_last":
-                # print("Torch requires channels_first, switching automatically")
+                print("Torch requires channels_first, switching automatically")
                 self.args.data_format = "channels_first"
 
 
