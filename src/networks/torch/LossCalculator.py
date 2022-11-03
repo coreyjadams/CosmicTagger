Fix some scaling hiccups when oversubscribing on a single GPU

src/networks/torch/LossCalculator.py
@@ -40,19 +40,22 @@ class LossCalculator(torch.nn.Module):
 
         loss   = self.segmentation_loss(labels_dict["segmentation"], network_dict["segmentation"])
         loss_metrics = {
-            "segmentation" : loss
+            "segmentation" : loss.detach()
         }
 
         if self.network_params.classification.active:
             event_loss = self.event_loss(labels_dict["event_label"], network_dict["event_label"])
-            loss_metrics["event_label"] = event_loss
-            loss      += self.network_params.classification.weight * event_loss
+            event_loss = event_loss * self.network_params.classification.weight
+            loss_metrics["event_label"] = event_loss.detach()
+            loss =  loss +  event_loss
 
         if self.network_params.vertex.active:
             vtx_loss   = self.vertex_loss(labels_dict["vertex"], network_dict["vertex"])
-            loss      += self.network_params.vertex.weight * vtx_loss
+            vtx_loss   = self.network_params.vertex.weight * vtx_loss
+            loss      +=  vtx_loss
 
         loss_metrics["total"] = loss
+
         return  loss, loss_metrics
 
     def vertex_loss(self, labels, logits):
@@ -120,10 +123,10 @@ class LossCalculator(torch.nn.Module):
                         weights[labels[i] == 1 ] = 1.5 * per_pixel_weight
                         weights[labels[i] == 2 ] = 10  * per_pixel_weight
 
-                total_weight = torch.sum(weights)
-                plane_loss = torch.sum(weights*plane_loss)
+                plane_loss = torch.mean(weights*plane_loss)
 
-                plane_loss /= total_weight
+                # total_weight = torch.sum(weights)
+                # plane_loss /= total_weight
 
             if loss is None:
                 loss = plane_loss

src/networks/torch/sparseuresnet3D.py
@@ -63,6 +63,7 @@ class SparseResidualBlock(nn.Module):
             filter_size = [nplanes,3,3],
             bias        = params.bias)
 
+        self._do_normalization = False
         if params.normalization == Norm.batch:
             self._do_normalization = True
             self.relu1 = scn.BatchNormLeakyReLU(outplanes)
@@ -78,7 +79,6 @@ class SparseResidualBlock(nn.Module):
             bias        = False)
 
         if params.normalization == Norm.batch:
-            self._do_normalization = True
             self.norm2 = scn.BatchNormalization(outplanes)
 
 
@@ -529,4 +529,4 @@ class UResNet3D(torch.nn.Module):
             return_dict["event_label"] = classified
 
 
-        return return_dict  
+        return return_dict

src/networks/torch/uresnet2D.py
@@ -530,7 +530,7 @@ class UResNet(torch.nn.Module):
                     n_filters = n_filters + params.n_initial_filters
 
             n_filters = 3*n_filters
-            
+
             self.classifier = BlockSeries(
                 inplanes = n_filters,
                 n_blocks = params.classification.n_layers,
@@ -593,7 +593,7 @@ class UResNet(torch.nn.Module):
         return_dict["segmentation"] = seg_labels
 
         if hasattr(self, "classifier"):
-            classified = classified.detach()
+            classification_head = classification_head.detach()
             classified = self.classifier(classification_head)
             classified = self.bottleneck_classifer(classified)
             # 4 classes of events:

src/utils/torch/distributed_trainer.py
@@ -109,7 +109,7 @@ class distributed_trainer(torch_trainer):
 
             self._rank = rank
             self._size = size
-            self._local_rank = local_rank
+            self._local_rank = int(local_rank)
 
             # It will want the master address too, which we'll broadcast:
             if rank == 0:
@@ -131,7 +131,11 @@ class distributed_trainer(torch_trainer):
             if self.args.run.compute_mode == ComputeMode.XPU:
                 # import torch_ccl
                 backend = 'ccl'
-            elif self.args.run.compute_mode == ComputeMode.GPU: backend = 'nccl'
+            elif self.args.run.compute_mode == ComputeMode.GPU:
+                backend = 'nccl'
+                if self.args.framework.oversubscribe:
+                    # nccl can have only 1 rank per GPU
+                    backend = 'gloo'
             elif self.args.run.compute_mode == ComputeMode.CPU: backend = 'gloo'
 
             # init_method = 'file:///home/cadams/ddp_init/ddp_init.txt'
@@ -151,7 +155,18 @@ class distributed_trainer(torch_trainer):
         if self._rank == 0:
             torch_trainer.save_model(self)
 
+    def target_device(self):
+        if self.args.framework.oversubscribe != 1:
+            if self.args.run.compute_mode == ComputeMode.GPU:
+                n_local_devices = torch.cuda.device_count()
+                return self._local_rank // self.args.framework.oversubscribe
+        else:
+            return self._local_rank
+
     def default_device_context(self):
+
+        # In some cases, we map multiple ranks onto the same target device:
+        target_device = self.target_device()
 
         # Convert the input data to torch tensors
         if self.args.run.compute_mode == ComputeMode.GPU:
@@ -159,11 +174,11 @@ class distributed_trainer(torch_trainer):
                 # Then, it's manually set, use it
                 return torch.cuda.device(0)
             else:
-                return torch.cuda.device(int(self._local_rank))
+                return torch.cuda.device(int(target_device))
         elif self.args.run.compute_mode == ComputeMode.XPU:
             # return contextlib.nullcontext
             try:
-                return ipex.xpu.device(int(self._local_rank))
+                return ipex.xpu.device(int(target_device))
             except:
                 pass
             return contextlib.nullcontext
@@ -179,14 +194,16 @@ class distributed_trainer(torch_trainer):
 
     def default_device(self):
 
+        target_device = self.target_device()
+
         if self.args.run.compute_mode == ComputeMode.GPU:
             if 'CUDA_VISIBLE_DEVICES' in os.environ:
                 # Then, it's manually set, use it
                 return torch.device("cuda:0")
             else:
-                return torch.device(f"cuda:{self._local_rank}")
+                return torch.device(f"cuda:{target_device}")
         elif self.args.run.compute_mode == ComputeMode.XPU:
-            device = torch.device(f"xpu:{self._local_rank}")
+            device = torch.device(f"xpu:{target_device}")
         elif self.args.run.compute_mode == ComputeMode.DPCPP:
             device = torch.device("dpcpp")
         else:
@@ -204,7 +221,6 @@ class distributed_trainer(torch_trainer):
         if self.args.framework.distributed_mode == DistributedMode.horovod:
             print(self._opt)
             self._opt = hvd.DistributedOptimizer(self._opt, named_parameters=self._net.named_parameters())
-            self._opt.param_groups[0]['capturable'] = True
         self.lr_scheduler = torch.optim.lr_scheduler.LambdaLR(self._opt, self.lr_calculator, last_epoch=-1)
 
 