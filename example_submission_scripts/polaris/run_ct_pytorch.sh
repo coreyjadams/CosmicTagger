803558e9d311629bde1e5319262e1d3ecf0d6ade
Corey Adams <corey.adams@anl.gov>
2022-11-08 19:30:45 +0000

    Update scripts

--- example_submission_scripts/polaris/run_ct_pytorch.sh
+++ example_submission_scripts/polaris/run_ct_pytorch.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-#PBS -l select=16:system=polaris
+#PBS -l select=8:system=polaris
 #PBS -l place=scatter
-#PBS -l walltime=3:00:00
-#PBS -q workq
+#PBS -l walltime=0:60:00
+#PBS -q debug-scaling
 #PBS -A datascience
+#PBS -l filesystems=home:grand
+
 
 # What's the cosmic tagger work directory?
 WORK_DIR=/home/cadams/Polaris/CosmicTagger
 cd ${WORK_DIR}
 
-DATA_DIR=/lus/grand/projects/datascience/cadams/datasets/SBND/
 
 # MPI and OpenMP settings
 NNODES=`wc -l < $PBS_NODEFILE`
@@ -18,27 +19,29 @@ NDEPTH=8
 
 let NRANKS=${NNODES}*${NRANKS_PER_NODE}
 
-LOCAL_BATCH_SIZE=1
+LOCAL_BATCH_SIZE=2
 let GLOBAL_BATCH_SIZE=${LOCAL_BATCH_SIZE}*${NRANKS}
 
-echo $GLOBAL_BATCH_SIZE
+echo "Global batch size: ${GLOBAL_BATCH_SIZE}"
 
 # Set up software deps:
-module load conda/2022-07-19
+module load conda/2022-09-08
 conda activate
 
 # Add-ons from conda:
-source /home/cadams/Polaris/polaris_conda_2022-07-19-venv/bin/activate
+source /home/cadams/Polaris/polaris_conda_2022-09-08-venv/bin/activate
 
 module load cray-hdf5/1.12.1.3
 
+# Env variables for better scaling:
+export NCCL_COLLNET_ENABLE=1
+export NCCL_NET_GDR_LEVEL=PHB
+
 mpiexec -n ${NRANKS} -ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind=depth \
 python bin/exec.py \
---config-name sn-at \
-run.id=polaris_sn-at_${GLOBAL_BATCH_SIZE}-light \
+run.id=event_id_${GLOBAL_BATCH_SIZE}_${NNODES} \
 run.distributed=True \
 run.minibatch_size=${GLOBAL_BATCH_SIZE} \
-data.data_directory=${DATA_DIR} \
 run.precision=mixed \
-mode.optimizer.loss_balance_scheme=light \
-run.iterations=5000
+run.iterations=5000 \
+framework=torch
